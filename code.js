064c1efec04c1190347c062cb39b40886d1a7472
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-02-09 12:13:39 -0600

    completed exercises #6 and #7

--- code.js
+++ code.js
@@ -3,11 +3,5 @@ function helloWorld() {
     return "Hello, World!";
 }
 function sayHello(name) {
-    if (name === "Alex") {
-        return "Hello, Alex";
-    }
-    else if (name === "Pat") {
-        return "Hello, Pat";
-    }
-    else return "Hello, Jane";
+    return "Hello, " + name;
 }
